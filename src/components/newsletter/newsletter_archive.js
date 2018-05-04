68c056702e65052181c8f7a93a3306b7bbcbefc1
Jake <kartchner56jake@gmail.com>
2018-05-04 13:46:50 -0600

    setup nesletter archive componenet

---
+++ src/components/newsletter/newsletter_archive.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NewsletterArchive extends Component {
+    render() {
+        return (
+            <ul>
+                <li>List item 0</li>
+                <li>List item 1</li>
+                <li>List item 2</li>
+            </ul>
+        )
+    }
+}
+
+export default NewsletterArchive;
